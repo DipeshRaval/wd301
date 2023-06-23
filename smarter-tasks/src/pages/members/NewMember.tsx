8ac94b0fde1ffc3395857a0292225c8aafca7e26
DipeshRaval <dipeshraval50@gmail.com>
2023-06-23 16:01:32 +0530

    add a id to submit btn for create a new member

--- smarter-tasks/src/pages/members/NewMember.tsx
+++ smarter-tasks/src/pages/members/NewMember.tsx
@@ -114,7 +114,7 @@ const NewMember = () => {
                         }`}
                       />
                       {errors.password && <span className='text-red-500'>This field is required</span>}
-                      <button type="submit" className="inline-flex justify-center rounded-md border border-transparent bg-blue-600 px-4 py-2 mr-2 text-sm font-medium text-white hover:bg-blue-500 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2">
+                      <button type="submit" id='create-member-btn' className="inline-flex justify-center rounded-md border border-transparent bg-blue-600 px-4 py-2 mr-2 text-sm font-medium text-white hover:bg-blue-500 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2">
                         Submit
                       </button>
                       <button type="submit" onClick={closeModal} className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2">
