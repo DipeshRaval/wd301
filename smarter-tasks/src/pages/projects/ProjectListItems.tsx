219b2bdf680c41a37297cc9bc210bae1bb2b770b
Dipesh Raval <103437774+DipeshRaval@users.noreply.github.com>
2023-07-29 07:18:19 +0530

    Update ProjectListItems.tsx

--- smarter-tasks/src/pages/projects/ProjectListItems.tsx
+++ smarter-tasks/src/pages/projects/ProjectListItems.tsx
@@ -13,9 +13,9 @@ export default function ProjectListItems() {
     return <span>Loading...</span>;
   }
 
-  if (projects.length === 0) {
-    throw Error("Error!!! Projects are not created Yet.");
-  }
+  // if (projects.length === 0) {
+  //  throw Error("Error!!! Projects are not created Yet.");
+  //}
 
 // Next, if there is an error, I'll show the error message.
   if (isError) {
